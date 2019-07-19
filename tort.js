e30ad713c52db166d5f988a0ed8d479529700d5b
Karl Lodholz <karllodholz@gmail.com>
2019-07-18 23:49:24 -0500

    new function playlists, which displays the available playlists.  Began work on the ability to add to the playlists and remove from them.

--- tort.js
+++ tort.js
@@ -17,6 +17,7 @@
  const TEST_CHANNEL_ID = 428015548325036032;
  const MUSIC_CHANNEL_ID = 598554308585455629;
  const HTML_URL_INDEX = "<li><div class=\"yt-lockup yt-lockup-tile yt-lockup-video vve-check clearfix\" data-context-item-id=\""
+ const PLAYLS_DIR = "./playlists/";
  var volume = INIT_VOL;
  var queue = [];
  var playing = false;
@@ -26,6 +27,7 @@
 
  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  //POTENTIAL ADD-ONS and problems
+ //remove colons from all commands because you know they are annoying
  //cant play mature content
  //cant play streams/live videos
  //rotating queue: will play one song from someone, then the next is from someone else
@@ -74,8 +76,7 @@
         message.channel.send("```help == displays list of commands\n\n"
                 + "search:Search_Request == returns the top result of the search request from YouTube\n\n"
                 + "search#:Search_Request == returns top results of search request from YouTube [1 < # < "+MAX_VIDS+"]\n\n"
-                + "play Search_Request == will find the first YouTube result and play that in the music voice channel\n\n"
-                + "play YouTube_URL == will play the YouTube video in the music voice channel\n\n"
+                + "play Search_Request/YouTube_URL == adds the first seach result or url to the queue and begins queue if not already playing\n\n"
                 + "play == unpauses the stream if paused\n\n"
                 + "pause == pauses the stream if playing\n\n"
                 + "skip == skips the current song in queue\n\n"
@@ -85,7 +86,11 @@
                 + "info == returns url of song playing\n\n"
                 + "ls || list == displays the names of the songs on the queue\n\n"
                 + "try again == restats the stream (use when bot just for some odd reason wont play)\n\n"
-                + "kill queue == kills the queue(includes current song playing)\n\n```");
+                + "kill queue == kills the queue(includes current song playing)\n\n"
+                + "playlists == displays the playlists available\n\n"
+                //+ "add Playlist_Name Search_Request/YouTube_URL == will add the search request to the playlist after asking for a confirmation on the search result.\n\n"
+                //+ "delete Playlist_Name Key_Word/Phrase == will remove all songs with Key_Word/Phrase upon confirmation.\n\n"
+                + "```");
     }
 
     //play command
@@ -234,9 +239,47 @@
                 message.channel.send("Invalid search request.  The search has been set to 1.");
         }
         var urlString = "";
-        getUTubeURL(search,numURLs,(urlString)=>{
+        getUTubeURL(search,numURLs,(urlString)=> {
             message.channel.send(urlString);
         });
+    }
+
+    //displays the playlists available
+    if(msg == "playlists") { 
+        let pylsStr = "Playlists:\n";
+        fs.readdir(PLAYLS_DIR, (err, files) => { 
+            files.forEach(file => {
+                pylsStr += file.substring(0,file.indexOf('.'));
+            });
+            if(pylsStr == "Playlists:\n")
+                pylsStr = "Im sorry, no playlists are available.  Please create a new playlist with the command `new playlist Playlist_Name`"; 
+            message.channel.send(pylsStr);
+        });
+    }
+
+    if(msg.startsWith("add ") || msg.startsWith("delete ")) {
+        let found = false;
+        let pyls = msg.substring(msg.indexOf(' ')+1);
+
+        pyls = pyls.substring(0,pyls.indexOf(' '));
+        if(pyls) { //will be false if no space is after the first space
+            fs.readdir(PLAYLS_DIR, (err, files) => { 
+                files.forEach(file => {
+                    if(pyls + ".txt" == file)
+                        found = true;  
+                });
+                if(found)
+                    fs.readFile(PLAYLS_DIR+pyls+".txt", function(err, data) {
+                        if(err) throw err;
+                        //console.log(data.toString());
+                        //request = msg.substring(msg.indexOf(pyls)+pyls)
+                    })
+                else
+                    message.channel.send("Im sorry, but the playlist: "+pyls+" could not be found.  Please try the command `playlists` to see available playlists.");
+            });
+        }
+        else
+            message.channel.send("Invallid command. See `help` for information on the commands");
     }
  }
 
