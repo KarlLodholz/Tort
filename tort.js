8cc617c5c6777426dd457a23d0428fdd3e1a98ee
Karl Lodholz <karllodholz@gmail.com>
2019-08-01 00:31:35 -0500

    permissions updated to administrators over owner of bot

--- tort.js
+++ tort.js
@@ -4,11 +4,9 @@
  const fs = require('fs')
  let jsonData = JSON.parse(fs.readFileSync('settings.json', 'utf-8'))
  const TOKEN = jsonData.token;
- 
+
  const bot = new Discord.Client();
  const BOT_ID = '541338820772233231';
- const OWNER_ID = '192461753542639617';
- const OWNER_ID2 = '323218204014936069';
  const MAX_VIDS = 5;
  const MAX_VOL = 100; //vol can go as high or low as desired, however anything other than 100 will distort the sound.  1000 sounds extremely distorted
  const INIT_VOL = 25;
@@ -16,7 +14,7 @@
  const YOUTUBE_URL_SEARCH = "https://www.youtube.com/results?search_query=";
  const TEST_CHANNEL_ID = jsonData.test_channel_id;
  const MUSIC_CHANNEL_ID = jsonData.command_channel_id;
- 
+
  const HTML_URL_INDEX = "<li><div class=\"yt-lockup yt-lockup-tile yt-lockup-video vve-check clearfix\" data-context-item-id=\""
  const PYLS_DIR = "./playlists/";
  var volume = INIT_VOL;
@@ -43,16 +41,16 @@
  }
 
  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
- 
+
  //bot.on('debug', console.log)
  bot.on('message',(message) => {
     //Stops bot from responding to itself
     if (message.author == bot.user) {
         return;
     }
-    
+
     //kill command: kill + @bot  //done this way to shut down multiple bots with one command.
-    if(message.author.id == OWNER_ID || message.author.id == OWNER_ID2) {
+    if(message.member.permissions.has('ADMINISTRATOR')) {
         if(message.content.startsWith('kill') && message.content.includes('@'+BOT_ID)) {
             message.channel.send("Terminating Program Tort\nGoodbye OwO");
             setTimeout(function() {
@@ -139,7 +137,7 @@
                         //message.channel.send("OwO has arrived");
                         connectionGlobal = connection;
                         Play(connectionGlobal,message.guild);
-                    }).catch(console.log); 
+                    }).catch(console.log);
                 });
             };
 
@@ -160,7 +158,7 @@
             Play(connectionGlobal,message.guild);
         }
     }
-    
+
     //resusmes stream
     if(msg == "play") {
         if(connectionGlobal && connectionGlobal.dispatcher)
@@ -180,7 +178,7 @@
             else
                 connectionGlobal.dispatcher.pause();
         else
-            message.channel.send("Something must be playing for me to pause it"); 
+            message.channel.send("Something must be playing for me to pause it");
     }
 
     //sets volume to requested number with bounds of 0 and MAX_VOL
@@ -189,7 +187,7 @@
             volume = command[1];
             updateVol(connectionGlobal);
         }
-        else    
+        else
             message.channel.send("Pick a valid number (0-"+MAX_VOL+")");
     }
 
@@ -213,7 +211,7 @@
         }
         else
             message.channel.send("There is nothing playing >//<");
-    
+
     //displays the queue
     if(msg == "ls" || msg == "list") {
         if(queue[0]) {
@@ -235,7 +233,7 @@
         var tempQ = [];
         for(let i = 0; i < queue.length; i++)
             tempQ[i] = new Song(queue[i].url,queue[i].name);
-        
+
         queue = [];
         connectionGlobal.dispatcher.end();
         message.member.voiceChannel.join()
@@ -258,25 +256,25 @@
     }
 
     //displays the playlists available
-    if(msg == "playlists") { 
+    if(msg == "playlists") {
         let pylsStr = "Playlists:\n";
-        fs.readdir(PYLS_DIR, (err, files) => { 
+        fs.readdir(PYLS_DIR, (err, files) => {
             files.forEach(file => {
                 pylsStr += file.substring(0,file.indexOf('.'));
             });
             if(pylsStr == "Playlists:\n")
-                pylsStr = "Im sorry, no playlists are available.  Please create a new playlist with the command `touch Playlist_Name`"; 
+                pylsStr = "Im sorry, no playlists are available.  Please create a new playlist with the command `touch Playlist_Name`";
             message.channel.send(pylsStr);
         });
     }
 
     //creating playlists
     if(command[0].toLowerCase() == "touch") {
-        fs.readdir(PYLS_DIR, (err, files) => { 
+        fs.readdir(PYLS_DIR, (err, files) => {
             var found = false;
             files.forEach(file => {
-                if(command[1].toLowerCase() + ".txt" == file) 
-                    found = true;  
+                if(command[1].toLowerCase() + ".txt" == file)
+                    found = true;
             });
             if(found)
                 message.channel.send("There is already a playlist named: "+command[1].toLowerCase());
@@ -291,18 +289,18 @@
 
     //deleting playlists
     if(command[0].toLowerCase() == "rm") {
-        fs.readdir(PYLS_DIR, (err, files) => { 
+        fs.readdir(PYLS_DIR, (err, files) => {
             var found = false;
             files.forEach(file => {
-                if(command[1].toLowerCase() + ".txt" == file) 
-                    found = true;  
+                if(command[1].toLowerCase() + ".txt" == file)
+                    found = true;
             });
             if(!found)
                 message.channel.send("There is no playlist named: "+command[1].toLowerCase());
             else {
                 fs.readFile(PYLS_DIR+command[1].toLowerCase()+".txt", function(err, data) {
                     let ownerID = data.toString().substring(0,data.toString().indexOf("\n"))
-                    if(ownerID == message.author.id || message.author.id == OWNER_ID || message.author.id == OWNER_ID2) {  //checks if the person deleting is the creator of the playlist or the admin
+                    if(ownerID == message.author.id || message.member.permissions.has('ADMINISTRATOR')) {  //checks if the person deleting is the creator of the playlist or the admin
                         fs.unlink(PYLS_DIR+command[1].toLowerCase()+".txt",(err) => {
                             if(err) throw err;
                             message.channel.send("Playlist: "+command[1]+" has been deleted");
@@ -320,10 +318,10 @@
         let found = false;
         //let command = msg.split(" "); //array of the commands split where spaces are present.
         if(command[1]) {
-            fs.readdir(PYLS_DIR, (err, files) => { 
+            fs.readdir(PYLS_DIR, (err, files) => {
                 files.forEach(file => {
-                    if(command[1].toLowerCase() + ".txt" == file) 
-                        found = true;  
+                    if(command[1].toLowerCase() + ".txt" == file)
+                        found = true;
                 });
                 if(found)
                     fs.readFile(PYLS_DIR+command[1].toLowerCase()+".txt", function(err, data) {
@@ -395,7 +393,7 @@
 
     if(command[0].toLowerCase() == "load") {
         if(command[1]) {
-            fs.readdir(PYLS_DIR, (err, files) => { 
+            fs.readdir(PYLS_DIR, (err, files) => {
                 let found = false;
                 files.forEach(file => {
                     if(command[1].toLowerCase()+".txt" == file)
@@ -414,7 +412,7 @@
                             .then(connection => {
                                 connectionGlobal = connection;
                                 Play(connectionGlobal,message.guild);
-                            }).catch(console.log); 
+                            }).catch(console.log);
                         });
                     }
                     else
@@ -431,7 +429,7 @@
 
     if(command[0].toLowerCase() == "ls" || command[0].toLowerCase() == "list") {
         if(command[1]) {
-            fs.readdir(PYLS_DIR, (err, files) => { 
+            fs.readdir(PYLS_DIR, (err, files) => {
                 let found = false;
                 files.forEach(file => {
                     if(command[1].toLowerCase()+".txt" == file)
@@ -479,7 +477,7 @@
                 guild.me.setNickname("");
             }
         });
-    } 
+    }
 }
 
  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -516,7 +514,7 @@
             numURLs = x;
 
             var urlString = ""; //turns array of urls into one string to send, to allow for smoother output.
-    
+
             for(var i = 0; i < numURLs; i++)
                 urlString += YOUTUBE_URL_VIDEO+address[i]+"\n";
 
@@ -539,7 +537,7 @@ Playlist</span></h3><div class="yt-lockup-byline ">YouTube</div><ol class="yt-lo
 
 */
 
- 
+
  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  function getYouTubeTitle(link,callback) {
     //console.log('!'+link);
@@ -556,17 +554,17 @@ Playlist</span></h3><div class="yt-lockup-byline ">YouTube</div><ol class="yt-lo
                 htmlString = htmlString.replace('&quot;',"\"");
 
             // ' conversion
-            while(htmlString.indexOf('&#39;') >= 0) 
+            while(htmlString.indexOf('&#39;') >= 0)
                 htmlString = htmlString.replace('&#39;',"\'");  // &#39 = '
 
             // \ conversion
-            while(htmlString.indexOf('\\') >= 0) 
+            while(htmlString.indexOf('\\') >= 0)
                 htmlString = htmlString.replace('\\',"~bksh~");   // \ = \\
             while(htmlString.indexOf('~bksh~') >= 0)
-                htmlString = htmlString.replace('~bksh~','\\\\') 
-            
+                htmlString = htmlString.replace('~bksh~','\\\\')
+
             // * conversion
-            while(htmlString.indexOf('*') >= 0 && htmlString.indexOf) 
+            while(htmlString.indexOf('*') >= 0 && htmlString.indexOf)
                 htmlString = htmlString.replace('*',"~star~");   // * = \*
             while(htmlString.indexOf('~star~') >= 0)
                 htmlString = htmlString.replace('~star~','\\*');
@@ -588,7 +586,7 @@ Playlist</span></h3><div class="yt-lockup-byline ">YouTube</div><ol class="yt-lo
  }
 
  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
- 
+
  //shuffles the queue of songs
  function shuffle(arr) {
     for (let i = arr.length - 1; i > 0; i--) {
@@ -599,12 +597,12 @@ Playlist</span></h3><div class="yt-lockup-byline ">YouTube</div><ol class="yt-lo
  }
 
  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
- 
+
  function bigPrint( str, callback) {
     let smallEnough = false; // = (nameStr.length < 1990) //discord has a 2000 word maximum per message
     let i = 0;
     let strArr = [];
-    do {  
+    do {
         if(str.length > 1990) {
             let goodsize = false;
             strArr[i] = "";
